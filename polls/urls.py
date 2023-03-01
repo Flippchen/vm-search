72a95cf9b33973c14c80798beb78d5fbb16e64f4
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-01 20:50:55 +0100

    added urls

---
+++ polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('csv', views.csv_to_table, name='csv_to_table'),
+path('login', views.ldap_login, name='login'),
+
+]
