749ff12101f75b2dedc84c36b5243590ce81ba7d
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-02 22:06:32 +0100

    added css

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import csv
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,10 +120,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/polls/login'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 def load_csv():
-    with open('data.csv', 'r') as f:
-        file = csv.reader(f)
+    with open('data3.csv', 'r') as f:
+        file = csv.reader(f, delimiter=';')
         my_data = [row for row in file]
     return my_data
 
