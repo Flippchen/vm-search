3f18020bf1e11aefa53aa47679b6a9b524765332
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-03 19:39:49 +0100

    format urls

--- vm/urls.py
+++ vm/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('csv', views.csv_to_table, name='csv_to_table'),
-path('login', views.ldap_login, name='login'),
-path('logout', views.logout_view, name='logout')
+    path('login', views.ldap_login, name='login'),
+    path('logout', views.logout_view, name='logout')
 
 ]
