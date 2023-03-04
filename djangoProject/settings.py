41a0aa648385adabf0022abbd60ff2b7615dda53
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-05 00:41:08 +0100

    edited allowed hosts

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-3$9oqmh9bvd03v43ivb!=wl(%tby*))!^=vb*otuax3)htg@tg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+# ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
