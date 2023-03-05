bf9fd114f728a87dc6e916dfd21627a9c0621357
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-05 01:01:03 +0100

    added comments to login

--- vm/views.py
+++ vm/views.py
@@ -40,39 +40,45 @@ def ldap_login(request):
         return render(request, 'login.html', {'form': form})
     elif request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
+        # Get the username and password provided by the user
         username = request.POST["username"]
         password = request.POST["password"]
-
+        # Authenticate against ldap server
         try:
             conn = ldap.initialize(settings.LDAP_SERVER)
             conn.simple_bind_s("{}@ad.Test.com".format(username), password)
+        # If authentication fails, return an 'invalid login' error message.
         except ldap.INVALID_CREDENTIALS:
             form.add_error(None, "Invalid username or password")
             return render(request, 'login.html', {'form': form})
-
+        # If authentication succeeds, search for user in ldap
         search_base = "OU=Test,dc=ad,dc=Test,dc=com"
         search_filter = "(sAMAccountName={})".format(username)
-
         results = conn.search_s(search_base, ldap.SCOPE_SUBTREE, search_filter)
+        # If user is not found, return an 'Invalid Group' error message.
         if len(results) == 0:
             form.add_error(None, "Invalid Group")
             return render(request, 'login.html', {'form': form})
-        else:
-            try:
-                groups = results[0][1]["memberOf"]
-                if any("TEST GROUP".encode("utf-8") in item for item in groups):
-                    try:
-                        user = User.objects.get(username=username)
-                    except:
-                        user = User.objects.create_user(username=username, password="123")
-                        user.save()
-                    user = authenticate(username=username, password="123")
-                    if user is not None:
-                        login(request, user)
-                    return redirect(reverse('csv_to_table'))
-            except:
-                form.add_error(None, "Invalid Group")
-                return render(request, 'login.html', {'form': form})
+        # If user is found, check if user is in the right group
+        try:
+            groups = results[0][1]["memberOf"]
+            if any("TEST GROUP".encode("utf-8") in item for item in groups):
+                # If user is in the right group, try to get user from database
+                try:
+                    user = User.objects.get(username=username)
+                except:
+                    # If user is not in database, create user
+                    user = User.objects.create_user(username=username, password="123")
+                    user.save()
+                # Authenticate against database
+                user = authenticate(username=username, password="123")
+                # Login user
+                if user:
+                    login(request, user)
+                return redirect(reverse('csv_to_table'))
+        except:
+            form.add_error(None, "Invalid Group")
+            return render(request, 'login.html', {'form': form})
     else:
         return HttpResponse("Invalid request")
 
